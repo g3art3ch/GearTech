c27606a8b1d8a8e466b315b0111fbe3d95b34fe0
Bruno Benetti <bruno.benetti@bnpsolucoes.com.br>
2024-08-28 12:50:28 -0300

    fix_sepa

---
+++ assets/pages_connected/saved-vehicle.php
@@ -0,0 +1,216 @@
+<?php
+include('connection.php');
+include('protect.php');
+include('connection_favorite.php');
+include('connection_cars.php');
+
+$nomeUSER = $_SESSION['nomeUsuario'];
+
+$sql_code1 = "SELECT * FROM favorites WHERE favoriteUSER = '$nomeUSER'";
+$sql_query2 = $favoriteDATA->query($sql_code1);
+$quantidade = $sql_query2->num_rows;
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../css/var.css">
+    <link rel="stylesheet" href="../css/main/main.css">
+    <link rel="stylesheet" href="../css/main/header.css">
+    <link rel="stylesheet" href="../css/user.css">
+    <link rel="stylesheet" href="../css/main/footer.css">
+    <link rel="shortcut icon" href="../icons/logo.ico" type="image/x-icon">
+    <title>Minha conta</title>
+</head>
+
+<body>
+    <main>
+        <div class="container">
+            <header>
+                <div class="area">
+                    <div class="logo">
+                        <a href="/Geartech/assets/pages_connected/connected.php">
+                            <img src="../images/logo.svg" alt="" />
+                        </a>
+                    </div>
+                    <div class="menu-opener">
+                        <div class="hamburger-icon">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </div>
+                        <div class="close-icon">
+                            <span></span>
+                            <span></span>
+                        </div>
+                    </div>
+                </div>
+                <nav>
+                <ul>
+                        <li><a href="./connected_catalog.php">Catálogo</a></li>
+                        <li><a href="">Manutenções</a></li>
+                        <li class="dropdown">
+                            <div class="user-enter">
+                                <img src="/GearTech/assets/icons/user.svg" alt="" class="user-photo">
+                                <a href="#" class="login-account"><?php echo $_SESSION['nomeUsuario']; ?></a>
+                                <img src="/GearTech/assets/icons/dowm-arrow.svg" alt="" onclick="toggleDropdown()">
+                            </div>
+                            <ul class="dropdown-menu">
+                                <li><a href="./saved-vehicle.php">Seus salvos</a></li>
+                                <li><a href="/GearTech/index.php">Sair</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <!-- <ul>
+                        <li>
+                            <a href="/GearTech/index.php" class="logout-area">
+                                <img src="../icons/logout.svg" alt="">
+                                <p>Encerrar sessão</p>
+                            </a>
+                        </li>
+                    </ul> -->
+                </nav>
+            </header>
+        </div>
+
+        <div class="right-side-user">
+                        <div class="card-user-saved-vehicle">
+                            <div class="headline-saved-vehicle">
+                                <div class="title-card-save-vehicle">
+                                    <img src="../icons/saved.svg" alt="">
+                                    <h2>Veículos salvos</h2>
+                                </div>
+                                <div class="comparative">
+                                    <a href="./comparative.php">Comparativo</a>
+                                </div>
+                            </div>
+
+                            <div class="slider-container"></div>
+
+                            <?php
+
+                            if ($quantidade > 0) {
+                                if (!isset($_SESSION)) {
+                                    session_start();
+                                }
+                                $_SESSION['resultados'] = array();
+                                while ($result = $sql_query2->fetch_assoc()) {
+                                    $_SESSION['resultados'][] = $result;
+                                }
+
+
+                            
+
+                                foreach ($_SESSION['resultados'] as $results) {
+                                    echo '<h2>' . $results['favoriteNAME'] . '</h2>';
+                                    $consultNAME = $results['favoriteNAME'];
+
+
+                                    $sql_code = "SELECT 
+    nc.nome,
+    fc.estilo,
+    oc.orcamento,
+    tc.combustivel,
+    cc.capacidade,
+    uc.tipoUso,
+    iden.idIden,
+    iden.urlCarro,
+    iden.Marca
+FROM 
+    nomeCarro nc
+INNER JOIN 
+    filtroCarros fc ON nc.idFiltro = fc.idFiltro
+INNER JOIN 
+    orcamentoCarro oc ON nc.idNome = oc.idNome
+INNER JOIN 
+    tipoCombustivel tc ON nc.idNome = tc.idNome
+INNER JOIN 
+    capacidadeCarro cc ON nc.idNome = cc.idNome
+INNER JOIN 
+    usoCarro uc ON nc.idNome = uc.idNome
+INNER JOIN 
+    identificador iden ON nc.idNome = iden.idNome  
+WHERE nome = '$consultNAME'";
+
+$sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL: " . $mysqli->error);
+$quantidade1 = $sql_query->num_rows;
+$_SESSION['FAVORITES'] = array();
+while ($resultFAVORITES = $sql_query->fetch_assoc()) {
+    $_SESSION['FAVORITES'][] = $resultFAVORITES;
+}
+
+                foreach ($_SESSION['FAVORITES'] as $fav) {
+
+
+
+                                    echo '<div class="area-save-vehicle">
+                                    <div class="box-image-save-vehicle">';
+                                    echo '<img src="../car_images/' . $fav['idIden'] .'.png" alt="">
+                                    </div>';
+                                    echo '<div class="info-save-vehicle">';
+                                    echo '<h2>' . $fav['nome'] . '</h2>';
+                                    echo    '<p class="price-save-vehicle">' . $fav['orcamento'] . '</p>';
+                                    echo '<p class="desc-save-vehicle">' . $fav['combustivel'] . '</p>';
+                                    echo '<p class="desc-save-vehicle">' . $fav['capacidade'] . '</p>';
+                                    echo '<p class="desc-save-vehicle">' . $fav['tipoUso'] . '</p>';
+                                    echo '</div>';
+                                    echo '</div>';
+                                }
+                            }
+
+                        }
+
+                            ?>
+
+                            <div class="icon-slider">
+                                <img id="toggleSlider" src="../icons/slider.svg" alt="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+    </main>
+    <footer>
+        <div class="container">
+            <div class="box-footer">
+                <div class="left-side-footer">
+                    <div class="links">
+                        <a href="">Termos de uso</a>
+                        <a href="">Catálogo</a>
+                        <a href="">Manutenções</a>
+                    </div>
+                    <div class="social-icons-footer">
+                        <a href=""><img src="/GearTech/assets/icons/instagram-footer.svg" alt=""></a>
+                        <a href=""><img src="/GearTech/assets/icons/email-footer.svg" alt=""></a>
+                    </div>
+                    <div class="mail-footer">
+                        Email: suporte.geartech@gmail.com
+                    </div>
+                </div>
+                <div class="right-side-footer">
+                    <h2>Sobre nós</h2>
+                    <p>Nós da GearTech compartilhamos nosso gosto por carros e somos dedicados a simplificar sua jornada de compra. Valorizamos a transparência e a confiabilidade, proporcionando a você a melhor escolha da sua vida.
+                    </p>
+                </div>
+            </div>
+            <div class="copy">
+                <a href="">© GearTech - Todos os direitos reservados</a>
+            </div>
+        </div>
+    </footer>
+
+
+
+    <script src="../js/script.js"></script>
+</body>
+
+</html>
