06ec09649d1f7c8825771cdd92cda407b6f1ea5e
Danilo Silva <danilo.mdo50@gmail.com>
2024-09-21 13:21:44 -0300

    commit

--- Banco/carstemp.sql
+++ Banco/carstemp.sql
@@ -19,6 +19,8 @@
 -- Table structure for table `carros`
 --
 
+USE carstemp
+
 DROP TABLE IF EXISTS `carros`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
